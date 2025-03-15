74759b2b7a7ef227f5afce9fd30c36e5d3b38af9
Drag <161552347+taslim19@users.noreply.github.com>
2025-03-16 02:22:55 +0530

    Update tagall.py

--- ubot/modules/tagall.py
+++ ubot/modules/tagall.py
@@ -1,14 +1,13 @@
 from ubot import *
 
 __MODULE__ = "Mention"
-__HELP__ = """
-Bantuan Untuk Mention
+__HELP__ = """Mention Help
 
-• Perintah: <code>{0}all</code> [type message/reply message]
-• Penjelasan: Untuk memention semua anggota grup dengan pesan yang anda inginkan.
+• Command: <code>{0}all</code> [type message/reply message]
+• Explanation: To mention all group members with the message you want.
 
-• Perintah: <code>{0}batal</code>
-• Penjelasan: Untuk membatalkan memention anggota grup.
+• Command: <code>{0}cancel</code>
+• Explanation: To cancel mentioning group members.
 """
 
 
