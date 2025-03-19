a4ec1fa71cfdbae50de48790dfdecd6e38b6ea8a
Drag <161552347+taslim19@users.noreply.github.com>
2025-03-19 22:53:09 +0530

    Update join.py

--- ubot/core/plugins/join.py
+++ ubot/core/plugins/join.py
@@ -7,7 +7,7 @@ async def join(client, message):
     Man = message.command[1] if len(message.command) > 1 else message.chat.id
     xxnx = await message.reply("Processing...")
     try:
-        await xxnx.edit(f"Berhasil bergabung ke : `{Man}`")
+        await xxnx.edit(f"Successfully joined : `{Man}`")
         await client.join_chat(Man)
     except Exception as ex:
         await xxnx.edit(f"ERROR: \n\n{str(ex)}")
@@ -17,16 +17,16 @@ async def leave(client, message):
     Man = message.command[1] if len(message.command) > 1 else message.chat.id
     xxnx = await message.reply("Processing...")
     if message.chat.id in BLACKLIST_CHAT:
-        return await xxnx.edit("Dilarang menggunakan perintah ini disni .")
+        return await xxnx.edit("You are not allowed to use this command here.")
     try:
-        await xxnx.edit_text(f"{client.me.first_name} Aku depresi .")
+        await xxnx.edit_text(f"{client.me.first_name} I'm sed .")
         await client.leave_chat(Man)
     except Exception as ex:
         await xxnx.edit_text(f"ERROR: \n\n{str(ex)}")
 
 
 async def kickmeall(client, message):
-    Man = await message.reply("Proses Pengeluaran Global Grup...")
+    Man = await message.reply(""Global Group Exit Process...")
     er = 0
     done = 0
     async for dialog in client.get_dialogs():
@@ -37,11 +37,11 @@ async def kickmeall(client, message):
                 await client.leave_chat(chat)
             except BaseException:
                 er += 1
-    await Man.edit(f"Berhasil keluar dari {done} grup, Gagal keluar dari {er} grup.")
+    await Man.edit(f"Successfully left {done} groups, failed to leave {er} groups.")
 
 
 async def kickmeallch(client, message):
-    Man = await message.reply("Proses Pengeluaran Global Channel...")
+    Man = await message.reply("Global Channel Exit Process...")
     er = 0
     done = 0
     async for dialog in client.get_dialogs():
@@ -53,5 +53,5 @@ async def kickmeallch(client, message):
             except BaseException:
                 er += 1
     await Man.edit(
-        f"Berhasil keluar dari {done} channel, Gagal keluar dari {er} channel."
+        f""Successfully left {done} channels, failed to leave {er} channels."
     )
