4ffd23904289b04cbeea23d28b6a7b4ecab3b7b8
Drag <161552347+taslim19@users.noreply.github.com>
2025-03-19 23:48:21 +0530

    Update adm.py

--- ubot/core/plugins/adm.py
+++ ubot/core/plugins/adm.py
@@ -14,13 +14,13 @@ async def admin_bannen(client, message):
     if message.command[0] == "kick":
         user_id, reason = await extract_user_and_reason(message)
         if not user_id:
-            return await message.reply_text("Tidak dapat menemukan pengguna.")
+            return await message.reply_text("user not found.")
         if user_id == (await client.get_me()).id:
-            return await message.reply_text("Tidak bisa menendang diri sendiri.")
+            return await message.reply_text("cannot kick yourself.")
         if user_id == OWNER_ID:
-            return await message.reply_text("Dia owner bot anda.")
+            return await message.reply_text("he/she is the owner of your bot.")
         if user_id in (await list_admins(message)):
-            return await message.reply_text("Tidak bisa menendang admin.")
+            return await message.reply_text("Cannot kick an admin.")
         mention = (await client.get_users(user_id)).mention
         await message.reply_to_message.delete()
         msg = f"<b>Kicked User :</b> {mention}\n<b>Admin :</b> {message.from_user.mention}"
@@ -36,13 +36,13 @@ async def admin_bannen(client, message):
     elif message.command[0] == "ban":
         user_id, reason = await extract_user_and_reason(message)
         if not user_id:
-            return await message.reply_text("Tidak dapat menemukan pengguna.")
+            return await message.reply_text("user not found .")
         if user_id == (await client.get_me()).id:
-            return await message.reply_text("Tidak bisa memblokir diri sendiri.")
+            return await message.reply_text("cannot kick yourself.")
         if user_id == OWNER_ID:
-            return await message.reply_text("Dia adalah Owner Bot anda.")
+            return await message.reply_text("he/she is the owner of your bot.")
         if user_id in (await list_admins(message)):
-            return await message.reply_text("Tidak dapat memblokir sesama admin.")
+            return await message.reply_text("Cannot kick an admin.")
         try:
             mention = (await client.get_users(user_id)).mention
         except IndexError:
@@ -63,13 +63,13 @@ async def admin_bannen(client, message):
     elif message.command[0] == "mute":
         user_id, reason = await extract_user_and_reason(message)
         if not user_id:
-            return await message.reply_text("Tidak dapat menemukan pengguna.")
+            return await message.reply_text("user not found .")
         if user_id == (await client.get_me()).id:
-            return await message.reply_text("Tidak dapat membisukan diri sendiri.")
+            return await message.reply_text("Cannot mute yourself.")
         if user_id == OWNER_ID:
-            return await message.reply_text("Dia Owner Bot Anda.")
+            return await message.reply_text("He/She is the owner of your bot.")
         if user_id in (await list_admins(message)):
-            return await message.reply_text("Tidak bisa membisukan sesama admin.")
+            return await message.reply_text("Cannot mute a fellow admin.")
         mention = (await client.get_users(user_id)).mention
         await message.reply_to_message.delete()
         msg = f"<b>Muted Users :</b> {mention}\n<b>Admin :</b> {message.from_user.mention}"
@@ -83,27 +83,27 @@ async def admin_bannen(client, message):
     elif message.command[0] == "unmute":
         user_id = await extract_user(message)
         if not user_id:
-            return await message.reply_text("Tidak dapat menemukan pengguna.")
+            return await message.reply_text("user not found.")
         try:
             mention = (await client.get_users(user_id)).mention
         except Exception as error:
             await message.reply(error)
         try:
             await message.chat.unban_member(user_id)
-            await message.reply(f"<b>{mention} sudah tidak bisu.</b>")
+            await message.reply(f"<b>{mention} no longer muted.</b>")
         except Exception as error:
             await message.reply(error)
     elif message.command[0] == "unban":
         user_id = await extract_user(message)
         if not user_id:
-            return await message.reply_text("Tidak dapat menemukan pengguna.")
+            return await message.reply_text(""Cannot find the user")
         try:
             mention = (await client.get_users(user_id)).mention
         except Exception as error:
             await message.reply(error)
         try:
             await message.chat.unban_member(user_id)
-            await message.reply(f"<b>{mention} sudah dapat bergabung.</b>")
+            await message.reply(f"<b>{mention} Able to join now .</b>")
         except Exception as error:
             await message.reply(error)
 
@@ -114,7 +114,7 @@ async def global_banned(client, message):
     cmd = message.command
     if not message.reply_to_message and len(cmd) == 1:
         await Tm.edit(
-            "Gunakan format: <code>gban</code> [user_id/username/balas ke user]"
+            "Gunakan format: <code>gban</code> [user_id/username/reply to the user]"
         )
     elif len(cmd) == 1:
         message.reply_to_message.from_user.id
@@ -123,7 +123,7 @@ async def global_banned(client, message):
     try:
         user = await client.get_users(user_id)
     except PeerIdInvalid:
-        await Tm.edit("Tidak dapat menemukan user tersebut.")
+        await Tm.edit("Cannot find that user.")
         return
     iso = 0
     gagal = 0
@@ -142,14 +142,14 @@ async def global_banned(client, message):
 
             if prik in DEVS:
                 return await Tm.edit(
-                    "Anda tidak bisa gban dia karena dia pembuat saya."
+                    "You cannot globally ban him/her because they are my creator."
                 )
             elif prik in prok:
                 return await Tm.edit(
-                    "Anda tidak bisa gban dia, karna dia adalah Admin Userbot Anda."
+                    "You cannot globally ban him/her because they are an Admin of your Userbot."
                 )
             elif udah:
-                return await Tm.edit("Pengguna ini sudah di gban.")
+                return await Tm.edit("This user has already been globally banned.")
             elif prik not in prok and prik not in DEVS:
                 try:
                     BANNED_USERS.add(prik)
@@ -164,8 +164,8 @@ async def global_banned(client, message):
         f"""
 <b>Global Banned</b>
 
-<b>Berhasil Banned: {iso} Chat</b>
-<b>Gagal Banned: {gagal} Chat</b>
+<b>Successfully Banned: {iso} Chat</b>
+<b>failed to ban: {gagal} Chat</b>
 <b>User: <a href='tg://user?id={prik}'>{user.first_name}</a></b>
 """
     )
@@ -180,7 +180,7 @@ async def cung_ban(client, message):
     cmd = message.command
     if not message.reply_to_message and len(cmd) == 1:
         await Tm.edit(
-            "Gunakan format: <code>ungban</code> [user_id/username/reply to user]"
+            "use format: <code>ungban</code> [user_id/username/reply to user]"
         )
     elif len(cmd) == 1:
         message.reply_to_message.from_user.id
@@ -189,7 +189,7 @@ async def cung_ban(client, message):
     try:
         user = await client.get_users(user_id)
     except PeerIdInvalid:
-        await Tm.edit("Tidak menemukan user tersebut.")
+        await Tm.edit("Could not find that user")
         return
     iso = 0
     gagal = 0
@@ -219,8 +219,8 @@ async def cung_ban(client, message):
         f"""
 <b>Global Unbanned</b>
 
-<b>Berhasil Unbanned: {iso} Chat</b>
-<b>Gagal UnBanned: {gagal} Chat</b>
+<b>Successful Unbanned: {iso} Chat</b>
+<b>Failed UnBanned: {gagal} Chat</b>
 <b>User: <a href='tg://user?id={prik}'>{user.first_name}</a></b>
 """
     )
@@ -230,7 +230,7 @@ async def gbanlist(client, message):
     gua = client.me.id
     total = await get_banned_count(gua)
     if total == 0:
-        return await message.reply("`Belum ada pengguna yang digban.`")
+        return await message.reply("No users have been globally banned yet.")
     nyet = await message.reply("`Processing...`")
     msg = "**Total Gbanned:** \n\n"
     tl = 0
@@ -245,7 +245,7 @@ async def gbanlist(client, message):
             msg += f"{tl}• {i}\n"
             continue
     if tl == 0:
-        return await nyet.edit("`Belum ada pengguna yang digban.`")
+        return await nyet.edit("No users have been banned yet.")
     else:
         return await nyet.edit(msg)
 
@@ -253,7 +253,7 @@ async def gbanlist(client, message):
 async def pin_message(client, message):
     mmk = await message.reply("<code>Processing...</code>")
     if not message.reply_to_message:
-        return await mmk.edit("Balas ke pesan untuk pin/unpin .")
+        return await mmk.edit("Reply to the message to pin/unpin .")
     r = message.reply_to_message
     if message.command[0][0] == "u":
         await r.unpin()
@@ -268,14 +268,14 @@ async def pin_message(client, message):
             disable_web_page_preview=True,
         )
     except ChatAdminRequired:
-        return await mmk.edit("<b>Anda bukan admin di group ini !</b>")
+        return await mmk.edit("<b>"You are not an admin in this group."!</b>")
 
 
 async def promotte(client, message):
     user_id = await extract_user(message)
     biji = await message.reply("<code>Processing...</code>")
     if not user_id:
-        return await biji.edit("Pengguna tidak ditemukan.")
+        return await biji.edit("user not found.")
     (await client.get_chat_member(message.chat.id, client.me.id)).privileges
     try:
         if message.command[0][0] == "f":
@@ -315,16 +315,16 @@ async def promotte(client, message):
         umention = (await client.get_users(user_id)).mention
         await biji.edit(f"Promoted! {umention}")
     except ChatAdminRequired:
-        return await biji.edit("<b>Anda bukan admin di group ini !</b>")
+        return await biji.edit("<b>You are not an admin in this group.!</b>")
 
 
 async def demote(client, message):
     user_id = await extract_user(message)
     sempak = await message.reply("<code>Processing...</code>")
     if not user_id:
-        return await sempak.edit("Pengguna tidak ditemukan")
+        return await sempak.edit("user not found ")
     if user_id == client.me.id:
-        return await sempak.edit("Tidak bisa demote diri sendiri.")
+        return await sempak.edit("cannot demote yourself.")
     await message.chat.promote_member(
         user_id,
         privileges=ChatPrivileges(
@@ -352,4 +352,4 @@ async def invite_link(client, message):
             link = await client.export_chat_invite_link(message.chat.id)
             await um.edit_text(f"**Link Invite:** {link}")
         except ChatAdminRequired:
-            return await biji.edit("<b>Anda bukan admin di group ini !</b>")
+            return await biji.edit("<b>you are not an admin in this group!</b>")
