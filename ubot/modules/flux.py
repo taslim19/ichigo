fc3c341c3acf3fd6ece90a2b04fadf1c836db496
Drag <161552347+taslim19@users.noreply.github.com>
2025-03-20 11:47:58 +0530

    Update flux.py

--- ubot/modules/flux.py
+++ ubot/modules/flux.py
@@ -5,6 +5,9 @@ __HELP__ = """Help For Flux
 • Explanation: Generates an image based on the given query using the FLUX API.
 
 """
+from ubot import PY
+
+
 @PY.UBOT("flux", sudo=True)
 async def _(client, message):
     await flux_func(client, message)
