bf395e1648e2ae9c629c91c4a233da89052ca997
Drag <161552347+taslim19@users.noreply.github.com>
2025-03-31 02:28:45 +0530

    Update vcplayer.py

--- ubot/modules/vcplayer.py
+++ ubot/modules/vcplayer.py
@@ -12,7 +12,7 @@ __HELP__ = """
   <b>• Description:</b> Skip current song
   
   <b>• Command:</b> <code>{0}end</code>
-  <b>• Description:</b> Stop music and clear playlist
+  <b>• Description:</b> Stop music and clear playlistt
 """
 
 from ubot import PY
